eabc5853b10e2e02f9168b67871188d8f1b85497
Ruiha Harawira <103089870+ruiha-harawira@users.noreply.github.com>
2022-06-17 10:23:52 +1200

    ru

--- client/components/Ru.jsx
+++ client/components/Ru.jsx
@@ -1,1 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+function Pixel () {
+
+  const randomColor = `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  const [style, setStyle] = useState ({
+    height: '20px',
+    width: '20px',
+    backgroundColour: randomColor
+  
+  })
+
+  const clickHandler = evt => {
+    evt.preventDefault()
+    setStyle({
+      height: '20px',
+      width: '20px',
+      backgroundColor: randomColor
+    })
+  }
+
+  const changeToTransparent = evt => {
+    evt.preventDefault() 
+    setStyle({
+      height: '20px',
+      width: '20px',
+      backgroundColour: '#00000000'
+
+    })
+  }
+
+  const make
+
+  return (
+    <div onClick={setInterval(clickHandler, 10000)} onDragEnter={changeToTransparent} draggable={true} onMouseEnter={makeGreen} style={style}>
+    </div>
+  )
+
+  
+  
+}
+export default Pixel 
+
