188f33e94ba605af241ccd81aa02966fab8c2561
yayuan_cassie <84106763+falicitt@users.noreply.github.com>
2022-06-17 11:07:45 +1200

    cams page pixels

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 function Nav () {
   return (
-    <div className='Nav'>
+    <div>
     <ul>
       <Link to='/'><li> Home </li></Link>
       <Link to='/Cam'><li> Cam </li></Link> 
