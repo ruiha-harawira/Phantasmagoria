ccd68f2289426dc8883cb55a95632fa41326fe0b
yayuan_cassie <84106763+falicitt@users.noreply.github.com>
2022-06-16 16:47:32 +1200

    basic setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Nav from './Nav'
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
+      <h1>Phantasmagoria</h1>
+      <Nav />
       <Routes>
-        <Route path='/test' element={<h1>TEST ROUTE</h1>} />
+        <Route path='/Cam' element={<h1>Cam's Route</h1>} />
+        <Route path='/Cassie' element={<h1>Cassie's Route</h1>} />
+        <Route path='/Ru' element={<h1>Ru's Route</h1>} />
       </Routes>
     </div>
   )
---
+++ client/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav () {
+  return (
+    <div>
+    <ul>
+      <li> Cam </li>
+      <li> Cassie</li>
+      <li> Ru </li>
+    </ul>
+</div>
+  )
+}
+
+export default Nav 
