8df8f9b498aa9e0ff72553e13c5e68385ea4079a
falicitt <falicity@qq.com>
2022-06-17 14:56:51 +1200

    add css to homepage

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,11 +7,11 @@ function Home() {
 
        <h1 className="glow">Phantasmagoria</h1>
        <div className='explanation'>
-        <p>/ˌfantazməɡɔːrɪə,ˌfantazməˈɡɒrɪə/</p>
-        <small>.noun</small>
-        <p>a sequence of real or imaginary images like that seen in a dream.</p>
+          <p>/ˌfantazməɡɔːrɪə,ˌfantazməˈɡɒrɪə/</p>
+          <small>.noun</small>
+          <p>a sequence of real or imaginary images like that seen in a dream.</p>
       </div>
-      </div>
+    </div>
 
   )
 }
