fe5ca0ed37850620fa84bd72b834c2b81e756d2b
Ruiha Harawira <103089870+ruiha-harawira@users.noreply.github.com>
2022-06-17 12:37:41 +1200

    added pixels

--- client/components/Ru.jsx
+++ client/components/Ru.jsx
@@ -1,52 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react'
+import RuPixel from './RuPixel'
 
-function RuPixel () {
-
-  const randomColor = `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
-  const [style, setStyle] = useState ({
-    height: '20px',
-    width: '20px',
-    backgroundColour: randomColor
-  
-  })
-
-  const clickHandler = evt => {
-    evt.preventDefault()
-    setStyle({
-      height: '20px',
-      width: '20px',
-      backgroundColor: randomColor
-    })
-  }
-
-  const hoverHandler = evt => {
-    evt.preventDefault() 
-    setStyle({
-      height: '20px',
-      width: '20px',
-      backgroundColour: '#00000000'
-
-    })
-  }
-
-
-
-  
-
+function Ru() {
   return (
-    <div 
-    style={style}
-    onClick= {clickHandler} 
-    onDragEnter={hoverHandler} 
-    draggable={true} 
-    onMouseEnter={hoverHandler}>
-
+    <div>
+      <div className="bean">
+        {Array.from({ length: 342 }, (_, i) => (
+          <RuPixel key={i} />
+        ))}
+      </div>
     </div>
   )
-
-  
-  
 }
-export default RuPixel 
 
+export default Ru
